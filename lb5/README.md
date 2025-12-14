4ac8a0660f7f19ac99cedf01d410dc7da466d431
RoenkoTimofey <roenkotimofii@gmail.com>
2025-12-14 14:05:05 +0000

    created

--- lb5/README.md
+++ lb5/README.md
@@ -1,4 +1,4 @@
-# Lb4
+# Lb5
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.0.2.
 
--- lb5/src/app/app.ts
+++ lb5/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { QuizComponent } from './quiz/quiz';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, QuizComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('lb4');
+  protected readonly title = signal('lb5');
 }
