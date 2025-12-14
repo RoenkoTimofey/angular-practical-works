11cddbba06a9c84d19a2424edcbf323e58e0a023
RoenkoTimofey <roenkotimofii@gmail.com>
2025-12-14 14:37:51 +0000

    create

--- lb6/README.md
+++ lb6/README.md
@@ -1,4 +1,4 @@
-# Lb5
+# Lb6
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.0.2.
 
--- lb6/src/app/app.ts
+++ lb6/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ConfigurableHighlightDirective } from './configurable-highlight.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ConfigurableHighlightDirective],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('lb5');
+  protected readonly title = signal('lb6');
 }
