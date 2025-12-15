76302b5398854907f00406600dad1d0c8a3e1596
RoenkoTimofey <roenkotimofii@gmail.com>
2025-12-15 22:54:09 +0200

    second

--- lb7/src/app/app.ts
+++ lb7/src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, Observable, filter, switchMap } from 'rxjs';
+import { ApiService, User, Post } from './api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('lb7');
+export class App implements OnInit {
+  users$: Observable<User[]> = this.api.getUsers();
+
+  private selectedUserId$ = new BehaviorSubject<number | null>(null);
+
+  posts$: Observable<Post[]> = this.selectedUserId$.pipe(
+    filter((id): id is number => id !== null),
+    switchMap((id) => this.api.getPostsByUserId(id))
+  );
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.selectUser(1);
+  }
+
+  selectUser(id: number | string): void {
+    this.selectedUserId$.next(Number(id));
+  }
 }
